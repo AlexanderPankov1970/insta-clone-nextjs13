d1fd764176407a49b71a9d2710dd6610b29e62ef
AlexanderPankov1970 <pankovsasha7@gmail.com>
2023-03-23 21:49:32 +0300

    add right side of the Header component

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import { SearchIcon } from "@heroicons/react/solid";
+import { AiFillHome, AiFillPlusCircle } from "react-icons/ai";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-7xl">
       <div className=" relative hidden sm:inline cursor-pointer m-3">
         <Image
           src={
@@ -17,7 +18,7 @@ export default function Header() {
           height={160}
         />
       </div>
-      <div className="relative sm:hidden cursor-pointer m-6">
+      <div className="relative sm:hidden cursor-pointer m-4">
         <Image
           src={
             "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
@@ -37,7 +38,17 @@ export default function Header() {
           className="pl-10 bg-gray-50 border-gray-500 text-sm  focus:ring-black focus:border-black rounded-lg"
         />
       </div>
-      <h1>Right sides</h1>
+      <div className="flex space-x-4 items-center mx-4">
+        <AiFillHome className="hidden md:inline text-2xl cursor-pointer hover:scale-125" />
+        <AiFillPlusCircle className="text-2xl cursor-pointer hover:scale-125" />
+        <Image
+          src={"/паньков 2.jpg"}
+          alt={"User Photo"}
+          className="h-12 rounded-xl"
+          width={50}
+          height={50}
+        />
+      </div>
     </div>
   );
 }
