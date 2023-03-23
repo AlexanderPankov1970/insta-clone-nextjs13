dff02761f2d9b606ae4f7d0a596a0ec103473cfe
AlexanderPankov1970 <pankovsasha7@gmail.com>
2023-03-23 14:56:29 +0300

    install next js 13 and Tailwind, create first template

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-blue-500">Hello</h1>
+      <h3 className="text-green-500">Welcome</h3>
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  );
+}
